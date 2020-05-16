e65835de73f5c928de2be437f217ac5d2c4fcf67
sen1000-byte <nishiwaki.chihiro@gmail.com>
2020-05-16 13:50:30 +0900

    Initial Commit

---
+++ PhotoMaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoMaster
+//
+//  Created by Chihiro Nishiwaki on 2020/05/16.
+//  Copyright © 2020 Nishiwaki sen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
